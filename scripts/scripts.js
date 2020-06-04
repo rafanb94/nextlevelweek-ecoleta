161476a8ad10d5eddffae7238106da1a20252be9
Rafael Brunner <rafael_nbc@hotmail.com>
2020-06-04 01:48:00 -0300

    update aula3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -59,8 +59,43 @@ else {
     cleanForm();
 }
 };
-/*
-function popularUfs() {
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+let selectedItems = []
+
+
+
+function handleSelectedItem(event){
+    const itemLi = event.target
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    //const alreadySelected = selectedItems.findIndex( item => item === itemId)
+    const alreadySelected = selectedItems.findIndex( item =>{
+        const itemFound = item == itemId
+        return itemFound
+    })
+    if(alreadySelected >= 0){
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferente = item != itemId
+            return itemIsDifferente
+        })
+        selectedItems = filteredItems
+    } else {
+        selectedItems.push(itemId)
+    }
+    collectedItems.value = selectedItems
+}
+
+
+
+/*function popularUfs() {
     const ufSelect = document.querySelector("select[name=uf]")
         try{
         fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
@@ -86,15 +121,17 @@ function getCities(){
     state.Input.value =  event.target.options[indexOfSelectedState].text
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
         try{
-           
+        citySelect.innerHTML = ""
+        citySelect.disable = true  
+
         fetch("url")
         .then( response => response.json())
         .then (cities => {
 
             for (city of cities){
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`   
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`   
         }
-        citySelect.disable = false
+
         })
     } catch (err){
         console.error(err)
