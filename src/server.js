d58ae91a16ae4b844160bd3878d9b62f05e09ec4
Rafael Brunner <rafael_nbc@hotmail.com>
2020-06-06 16:07:31 -0300

    aula 5 - final

--- src/server.js
+++ src/server.js
@@ -9,17 +9,70 @@ nunjucks.configure("src/views",{
     express:server,
     noCache: true
 } )
+
+//configurar para leitura da pasta public
 server.use(express.static("public"))
+
+//habilitar o uso do request.body da nossa aplicação
+server.use(express.urlencoded({ extended: true }))
 
 server.get("/", (request, response) =>{
    return response.render("index.html")
 })
-server.get("/cadastrar-ponto-de-coleta", (request, response) =>{
+server.get("/cadastrar-ponto-de-coleta", (request, response) =>{ 
+    //request.query: Query Strings da nossa URL, normalmente são os campos enviados por algum input/formulário
+    //console.log(req.query)
     return response.render("create-point.html")
 })
-server.get("/resultado-pontos-de-coleta", (request, response) =>{
+server.post("/cadastrar-ponto-de-coleta", (request, response) =>{ 
+    //request.body: retorna o "corpo do formulário" ou seja, os dados enviados pelo input
 
-    db.all(`SELECT * FROM places`, function(err, rows){
+    const query = `
+    INSERT INTO places (
+        image,
+        name,
+        address,
+        cep,
+        state,
+        city,
+        items
+    ) VALUES (?,?,?,?,?,?,?);
+`
+    const values = [
+        request.body.image,
+        request.body.name,
+        request.body.address,
+        request.body.cep,
+        request.body.state,
+        request.body.city,
+        request.body.items
+    ]
+
+    function afterInsertData(err){
+        if (err){
+           console.error(err)
+           return response.send("erro no cadastro")
+        }
+        console.log("Cadastro com sucesso")
+        console.log(this)//referencia a resposta do run nesta função
+        return response.render("/cadastrar-ponto-de-coleta", {saved: true})
+    }
+    db.run(query, values, afterInsertData)
+
+
+})
+
+
+server.get("/resultado-pontos-de-coleta", (request, response) =>{
+    
+    const search = request.query.search
+
+    if(search == ""){
+    
+    return response.render("search-results.html", {total: 0})
+    }
+
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows){
         if(err){
             console.error(err)
         }
